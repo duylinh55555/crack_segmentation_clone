06b24e8e4c72bad45a92cff7460f370f09eee7c4
duylinh55555 <linh1999duy@gmail.com>
2022-07-14 17:29:50 +0700

    modify for colab #1

---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+gdown https://drive.google.com/uc?id=1xrOqv0-3uMHjZyEUrerOYiYXW_E8SUMP
+gdown https://drive.google.com/uc?id=1wA2eAsyFZArG3Zc9OaKvnBuxSAPyDl08 -O "./models/model_unet_vgg_16_best.pt"
