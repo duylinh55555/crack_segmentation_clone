642b3cfa535a9e38dc36a3582907befb2b60d642
duylinh55555 <linh9920duy@gmail.com>
2022-07-15 02:44:33 +0000

    Add setup file

--- setup.py
+++ setup.py
@@ -1,2 +1,10 @@
-import subprocess
-subprocess.call(['sh', 'setup.py'])
+# import subprocess
+# subprocess.call(['sh', 'setup.py'])
+
+import os
+
+os.system("sudo apt install git-all")
+os.system("gdown https://drive.google.com/uc?id=1xrOqv0-3uMHjZyEUrerOYiYXW_E8SUMP")
+os.system("mkdir ./models")
+os.system("gdown https://drive.google.com/uc?id=1wA2eAsyFZArG3Zc9OaKvnBuxSAPyDl08 -O ""./models/model_unet_vgg_16_best.pt""")
+# os.system("")
